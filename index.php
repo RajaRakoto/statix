3e896b612fce75eb384a3afefb459f4d8787f3c0
raja <raja.rakoto7@gmail.com>
2021-07-08 21:52:32 +0300

    [add]: guide/php-note | [edit]: index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php 
-echo "hello!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="project" content="statix">
+  <meta name="description" content="intranet applcation">
+  <meta name="keywords" content="stat,web,app,statistique,centre,multimedia">
+  <meta name="author" content="Raja,Kanto"> 
+  <title>STATIX APPS</title>
+</head>
+<body>
+
+</body>
+</html>
