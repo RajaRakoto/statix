188b7dc7baafca3f855bcd1665dff895e6e87036
raja <raja.rakoto7@gmail.com>
2021-07-10 16:52:40 +0300

    [database]: import dbstatix_connect.php | [feature]: insert_data | [cdn]: import modal cdn | [html]: table structure

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<!-- PHP CORE (BEGIN)-->
+  <?php
+    //IMPORT DBSTATIX (test connexion) 
+    require('dbstatix_connect.php');
+
+    //DEFINE all notif default value
+    $notif = '';
+    $notifClass = '';
+  ?>
+
+  <?php
+    //INSERTION (notification test)
+    if(isset($_POST['submit'])){ //ecouteur d'evenement (sur le boutton "SUBMIT")
+      //recuperation de la valeur par $_POST
+      $nom = $_POST['nom']; 
+      $prenom = $_POST['prenom'];
+      $activite = $_POST['activite'];
+      $etablissement = $_POST['etablissement'];
+      $filiere = $_POST['filiere'];
+      $niveau = $_POST['niveau'];
+      
+      //ajout de l'entrer au base de donnees
+      $sql = "INSERT INTO statix_database(nom, prenom, activite, etablissement, filiere, niveau) VALUES('$nom', '$prenom', '$activite', '$etablissement', '$filiere', '$niveau')";
+
+      //OUTPUT (result)
+      $result = mysqli_query($statix_connexion, $sql); //retourne une valeur boolean
+      if($result) { //si les donnees ont ete bien inserE dans la base de donnees
+        $notif = "INSERTION avec success !"; //message
+        $notifClass="alert-success"; //change class (HTML+CSS)
+      }
+      else { //si les donnees n'ont pas ete inserE dans la base de donnees
+        $notif = "ERREUR d'insertion !"; //message
+        $notifClass="alert-danger"; //change class (HTML+CSS)
+      }
+    }
+  ?>
+  
+  <?php
+    //DELETE (notification test)
+    
+  ?>
+<!-- PHP CORE (END)-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +50,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1. 0">
     <meta name="project" content="statix">
-    <meta name="description" content="intranet application">
-    <meta name="keywords" content="stat,web,app,statistique,centre, multimedia,chart,graph">
+    <meta name="description" content="intranet web application">
+    <meta name="keywords" content="stat,web,app,statistique,centre,multimedia,chart,graph">
     <meta name="author" content="Raja RAKOTONIRINA,Kanto RAMANANDRAIBE"> 
 
   <!-- CDN IMPORT -->
@@ -19,13 +62,15 @@
     <!-- font awesome CDN -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 
-
     <!-- bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <!-- -->
+    
+    <!-- modalcss CDN -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
   
-  <!-- CSS IMPORT -->
+  <!-- CSS LINK -->
     <link rel="stylesheet" href="style.css" />
   
   <!-- TITLE -->
@@ -39,22 +84,33 @@
     <h2 id="head"><span id="statix-text">STATIX</span> table</h2>
   </header>
   
-  
   <!-- MAIN -->
   <main>
 
     <div class="container">
 
+        <!-- NOTIFICATION - insertion|suppression (generator) -->
+          
+
         <!-- MODEL0 (button) -->
           <button type="button" class="btn btn-info btn-sm mb-5" style="float:right" data-toggle="modal" data-target="#myModal">INPUT</button>
 
-      <table id="table_id" class="display">
+      <table id="statix_table" class="display responsive nowrap" cellspacing="0" width="100%">
+        <!-- ENTETE DU TABLEAU -->
         <thead>
           <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
+            <th>No#</th>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Activite</th>
+            <th>Etablissement</th>
+            <th>Filiere</th>
+            <th>Niveau</th>
+            <th>Date</th>
+            <th>MANIPULATION</th>
           </tr>
         </thead>
+        <!-- CORPS DU TABLEAU (generE auto par php&mysql)-->
         <tbody>
           <tr>
             <td>Row 1 Data 1</td>
